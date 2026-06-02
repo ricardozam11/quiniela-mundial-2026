8bdd8ab110f3ae8b82432141422ff6151945ec76
ricardozam11 <ricardozam11@gmail.com>
2026-06-02 12:35:02 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArxDLN6fVYDRcGPq30NrO5iCybuXtOQ0Y",
+  authDomain: "quiniela-mundial-2026-613a0.firebaseapp.com",
+  projectId: "quiniela-mundial-2026-613a0",
+  storageBucket: "quiniela-mundial-2026-613a0.firebasestorage.app",
+  messagingSenderId: "273037919356",
+  appId: "1:273037919356:web:66817f3db573765cfcb5f9"
+};
+
+export const app = initializeApp(firebaseConfig);
